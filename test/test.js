5a32b1d69d8ea7840e7172ead80a040217337734
Macpro <ngoc__thao@hotmail.com>
2019-11-28 14:55:29 +0700

    Update test case to be fail

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
 describe('GET /', () => {
  it('respond with hello world', (done) => {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ request(app).get('/').expect('hello world 123', done);
  });
 });
